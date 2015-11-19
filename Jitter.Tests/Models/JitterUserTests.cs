2e488ab965c8f7957e8e60c848e331be4a41d0d8
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-11-19 10:11:12 -0800

    New JitterUser Model with tests. Added Data Annotations to Model

---
+++ Jitter.Tests/Models/JitterUserTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jitter.Models;
+
+namespace Jitter.Tests.Models
+{
+    [TestClass]
+    public class JitterUserTests
+    {
+        [TestMethod]
+        public void JitterUserEnsureICanCreateInstance()
+        {
+            JitterUser a_user = new JitterUser();
+            Assert.IsNotNull(a_user);
+        }
+
+        [TestMethod]
+        public void JitterUserEnsureJitterUserHasAllTheThings()
+        {
+            // Arrange
+            JitterUser a_user = new JitterUser();
+            a_user.Handle = "adam1";
+            a_user.FirstName = "Adam";
+            a_user.LastName = "Sandler";
+            a_user.Picture = "https://google.com";
+            a_user.Description = "I am Awesome!";
+
+            // Assert 
+            Assert.AreEqual("I am Awesome!", a_user.Description);
+            Assert.AreEqual("adam1", a_user.Handle);
+            Assert.AreEqual("Adam", a_user.FirstName);
+            Assert.AreEqual("Sandler", a_user.LastName);
+            Assert.AreEqual("https://google.com", a_user.Picture);
+
+        }
+    }
+}
---
+++ Jitter/Models/JitterUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Jitter.Models
+{
+    public class JitterUser
+    {
+        [Key]
+        public int JitterUserId { get; set; }
+
+        [MaxLength(161)]
+        public string Description { get; set; }
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        [MinLength(3)]
+        [RegularExpression(@"^[a-zA-Z\d]+[-_a-zA-Z\d]{0,2}[a-zA-Z\d]+")]
+        public string Handle { get; set; }
+
+        public string LastName { get; set; }
+        public string Picture { get; set; }
+    }
+}
