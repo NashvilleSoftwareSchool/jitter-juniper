a9a66bb2de80e8f9a56d0865da05f9841e353489
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-11-24 10:05:03 -0800

    Implemented GetUserByHandle, IsHandleAvailable and SearchByHandle

--- Jitter/Models/JitterRepository.cs
+++ Jitter/Models/JitterRepository.cs
@@ -25,5 +25,43 @@ namespace Jitter.Models
             var query = from users in _context.JitterUsers select users;
             return query.ToList();
         }
+
+        public JitterUser GetUserByHandle(string handle)
+        {
+            // SQL: select * from JitterUsers AS users where users.Handle = handle;
+            var query = from user in _context.JitterUsers where user.Handle == handle select user;
+            // IQueryable<JitterUser> query = from user in _context.JitterUsers where user.Handle == handle select user;
+            // We need to make sure there's exactly one user returned. hmmmm.
+
+            return query.SingleOrDefault();
+        }
+
+        public bool IsHandleAvailable(string handle)
+        {
+            bool available = false;
+            try
+            {
+                JitterUser some_user = GetUserByHandle(handle);
+                if (some_user == null)
+                {
+                    available = true;
+                }
+            }
+            catch (InvalidOperationException){}
+
+            return available;
+        }
+
+        public List<JitterUser> SearchByHandle(string handle)
+        {
+            // SQL: select * from JitterUsers As users where users.Handle like '%handle%';
+            // handleblah
+            // blahhandle
+            // blahhandleblah
+            var query = from user in _context.JitterUsers select user;
+            List<JitterUser> found_users = query.Where(user => user.Handle.Contains(handle)).ToList();
+            found_users.Sort();
+            return found_users;
+        }
     }
 }
