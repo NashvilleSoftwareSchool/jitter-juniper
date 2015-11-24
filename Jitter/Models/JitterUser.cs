60b003e01b2a5da429216a03ddc1f3260879bf20
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-11-24 10:05:48 -0800

    Implemented the ICompariable Interface to get Sort on List<JitterUser> working.

--- Jitter/Models/JitterUser.cs
+++ Jitter/Models/JitterUser.cs
@@ -6,7 +6,7 @@ using System.Web;
 
 namespace Jitter.Models
 {
-    public class JitterUser
+    public class JitterUser : IComparable
     {
         [Key]
         public int JitterUserId { get; set; }
@@ -27,6 +27,17 @@ namespace Jitter.Models
         // ICollection, IEnumerable, IQueryable
         public List<Jot> Jots { get; set; }
         public List<JitterUser> Following { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            // Let's sort user based on their Handles b/c handles are strings AND...
+            // .NET knows how to compare strings already. ha!
+
+            // We need to explicitly cast from object type to JitterUser Type
+            JitterUser other_user = obj as JitterUser;
+            int answer = this.Handle.CompareTo(other_user.Handle);
+            return answer;
+        }
         //public List<JitterUser> Followers { get; set; } // Again, this is just one way to do this. Not the only way.
     }
 }
