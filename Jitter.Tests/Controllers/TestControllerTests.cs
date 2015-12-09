31272835f1a2c9d2f239fb5bf709bc95e0916fe5
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-12-09 10:19:56 -0800

    Added Sample WebAPI controller

---
+++ Jitter.Tests/Controllers/TestControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using Jitter.Controllers;
+
+namespace Jitter.Tests.Controllers
+{
+    [TestClass]
+    public class TestControllerTests
+    {
+        [TestMethod]
+        public void TestControllerEnsureICanCallGetAction()
+        {
+            // Arrange
+            TestController my_controller = new TestController();
+            string expected_output = "Hello, is it me you're looking for?";
+
+            // Act
+            string actual_output = my_controller.Get();
+
+            // Assert
+            Assert.AreEqual(expected_output, actual_output);
+        }
+    }
+}
